182bd70021e3381af8ecd27100957f9c1b7befd9
hannah <hannahjmccain@gmail.com>
2023-02-25 11:37:48 -0700

    test2

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -31,7 +31,7 @@ const Layout: React.FC<LayoutProps> = ({ children }: LayoutProps) => {
   return (
     <div
       className={classNames(
-        "bg-black xl:bg-primary-200",
+        "bg-black md:bg-blue-50",
         "h-screen flex flex-col transition-all relative"
       )}>
       <span className="lg:hidden inline">
